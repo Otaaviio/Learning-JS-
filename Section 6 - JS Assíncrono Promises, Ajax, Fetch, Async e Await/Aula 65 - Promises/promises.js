4ea8fd55487105c326831a800498e3bd1a57b717
Otaaviio <inabaotavio7@gmail.com>
2025-07-16 13:48:09 -0300

    add promises example with random delay and error handling

---
+++ Section 6 - JS Assíncrono Promises, Ajax, Fetch, Async e Await/Aula 65 - Promises/promises.js
@@ -0,0 +1,31 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function espera(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    if (typeof msg !== "string") reject("BAD VALUE");
+
+    setTimeout(() => {
+      resolve(msg);
+    }, tempo);
+  });
+}
+
+espera("Estabelecendo conexão", rand(1, 3))
+  .then((resposta) => {
+    console.log(resposta);
+    return espera("Buscando dados da BASE", rand(1, 3));
+  })
+  .then((resposta) => {
+    console.log(resposta);
+    return espera(1, rand(1, 3));
+  })
+  .then(() => {
+    console.log("Exibe dados na tela");
+  })
+  .catch((e) => {
+    console.log(`erro: ${e}`);
+  });
